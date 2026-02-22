d295b1fed6e2333a05c23fca1a978a36b450c15d
sarva1210 <sarva.vish77@gmail.com>
2026-02-22 14:23:19 +0530

    routes

---
+++ src/routes/Mainroutes.jsx
@@ -0,0 +1,20 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "../pages/Home";
+import Recipes from "../pages/Recipes";
+import About from "../pages/About";
+import Create from "../pages/Create";
+import SingleRecipe from "../pages/SingleRecipe";
+
+const Mainroutes = ()=>{
+    return (
+        <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/recipes" element={<Recipes/>}/>
+            <Route path="/recipes/details/:id" element={<SingleRecipe/>}/>
+            <Route path="/create-recipe" element={<Create/>}/>
+            <Route path="/about" element={<About/>}/>
+        </Routes>
+    )
+}
+
+export default Mainroutes
