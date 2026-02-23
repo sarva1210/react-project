8eb641cd496b0a329644bf3bbecfa2c5a5934609
sarva1210 <sarva.vish77@gmail.com>
2026-02-23 09:30:47 +0530

    initialize

--- src/context/RecipeContext.jsx
+++ src/context/RecipeContext.jsx
@@ -1,25 +1,12 @@
-import { createContext, useState } from "react"
+import { createContext, useEffect, useState } from "react"
 
 export const recipecontext = createContext(null)
 
 const RecipeContext = (props)=>{
-    const [data, setdata] = useState([
-
-        {"id":1,
-            "title":"Classic Margherita Pizza",
-            "ingr":[
-                "Pizza dough","Tomato sauce","Fresh mozzarella cheese","Fresh basil leaves","Olive oil","Salt and pepper to taste"
-            ],
-            "inst":[
-                "Preheat the oven to 475°F (245°C).","Roll out the pizza dough and spread tomato sauce evenly.","Top with slices of fresh mozzarella and fresh basil leaves.","Drizzle with olive oil and season with salt and pepper.","Bake in the preheated oven for 12-15 minutes or until the crust is golden brown.","Slice and serve hot."
-            ],
-            "image":"https://cdn.dummyjson.com/recipe-images/1.webp",
-            "chef":"Pankaj chamoli",
-            "category":"dinner",
-            "desc":"Make Italian style pizza at home"
-        }
-
-    ])
+    const [data, setdata] = useState([])
+    useEffect(()=>{
+        setdata(JSON.parse(localStorage.getItem("recipes")) || [])
+    },[])
     console.log(data)
     return(
         <recipecontext.Provider value={{data, setdata}}>{props.children}</recipecontext.Provider>
@@ -27,3 +14,18 @@ const RecipeContext = (props)=>{
 }
 
 export default RecipeContext
+
+
+// {"id":1,
+//             "title":"Classic Margherita Pizza",
+//             "ingr":[
+//                 "Pizza dough","Tomato sauce","Fresh mozzarella cheese","Fresh basil leaves","Olive oil","Salt and pepper to taste"
+//             ],
+//             "inst":[
+//                 "Preheat the oven to 475°F (245°C).","Roll out the pizza dough and spread tomato sauce evenly.","Top with slices of fresh mozzarella and fresh basil leaves.","Drizzle with olive oil and season with salt and pepper.","Bake in the preheated oven for 12-15 minutes or until the crust is golden brown.","Slice and serve hot."
+//             ],
+//             "image":"https://cdn.dummyjson.com/recipe-images/1.webp",
+//             "chef":"Pankaj chamoli",
+//             "category":"dinner",
+//             "desc":"Make Italian style pizza at home"
+//         }
