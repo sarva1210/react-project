2de633ab7ab218cd4080c5cb41065f4629ae29fc
sarva1210 <sarva.vish77@gmail.com>
2026-02-23 09:31:00 +0530

    routes

--- src/routes/Mainroutes.jsx
+++ src/routes/Mainroutes.jsx
@@ -4,6 +4,8 @@ import Recipes from "../pages/Recipes";
 import About from "../pages/About";
 import Create from "../pages/Create";
 import SingleRecipe from "../pages/SingleRecipe";
+import PageNotFound from "../pages/PageNotFound";
+import Fav from "../pages/Fav";
 
 const Mainroutes = ()=>{
     return (
@@ -13,6 +15,8 @@ const Mainroutes = ()=>{
             <Route path="/recipes/details/:id" element={<SingleRecipe/>}/>
             <Route path="/create-recipe" element={<Create/>}/>
             <Route path="/about" element={<About/>}/>
+            <Route path="/fav" element={<Fav/>}/>
+            <Route path="/*" element={<PageNotFound/>}/>
         </Routes>
     )
 }
